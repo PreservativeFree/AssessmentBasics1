0f67ad3735ce26f6b1710c3c4d4edf04e28b2885
Nelson Moreno <thegreyabove@gmail.com>
2022-03-04 11:21:41 -0500

    initial commit

--- sldc.js
+++ sldc.js
@@ -5,17 +5,23 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
 /*
-    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of 
+    each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+const analysisOfRequirements = "This is the second step, where you need to understand and document what is required by users and other stakeholders";
+const design = "This is the third step, where you develop a design around solutions to the problems presented by requirements, often involving process models and storyboards";
+const implementation = "This is the fourth step, where the actual coding takes place and active energy and fullfillment procedures take effect into the actual program itself";
+const testingAndIntegration = "This is the fifth step, where pre-planned scenarios are part of software design and coding - and conducting performance testing to simulate load testing on the application";
+const maintain = "This is the final step, to be able to maintain quality and delivery over the applications lifecycle, and to evaluate the development process with models";
